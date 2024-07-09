5fb5085e54f38d696a53a841d88cafd51b816f27
Cyan <cyanbun96@gmail.com>
2024-07-10 01:00:01 +0300

    LFSR Toy cleanup

--- doc/lfsr_toy/lfsr_toy.c
+++ doc/lfsr_toy/lfsr_toy.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <string.h>
 
 #define SCR_W 60
 #define SCR_H 13
@@ -11,6 +8,7 @@
 #define Y_POS 5
 
 int lfsr_len = 7;
+unsigned int mask = 0;
 unsigned int lfsr = 1337;
 unsigned int tap1 = 2;
 unsigned int tap2 = 3;
@@ -31,7 +29,7 @@ void drawScreen(){
 }
 
 void initScreen(){
-	memset(sb, ' ', SCR_H * SCR_W);
+	// Borders
 	sb[0][0] = '#';
 	sb[SCR_H - 1][0] = '#';
 	sb[SCR_H - 1][SCR_W - 1] = '#';
@@ -47,6 +45,7 @@ void initScreen(){
 	for(int i = 1; i < SCR_W - 1; ++i)
 		sb[Y_POS + 4][i] = '-';
 
+	// Register
 	sb[Y_POS + 3][X_POS] = '|';
 	sb[Y_POS + 4][X_POS] = '^';
 	for(int i = 0; i < lfsr_len; ++i){
@@ -64,6 +63,7 @@ void initScreen(){
 	sprintf(sb[Y_POS + 3] + X_POS + 1, "Register");
 	sb[Y_POS + 3][X_POS + 9] = ' ';
 
+	// XOR
 	sb[Y_POS - 5][X_POS] = 'T';
 	sb[Y_POS - 5][X_POS + 8] = 'T';
 	sb[Y_POS - 4][X_POS] = '+';
@@ -71,11 +71,6 @@ void initScreen(){
 	for(int i = 1; i < 8; ++i){
 		sb[Y_POS - 4][X_POS + i] = '-';
 	}
-	for(int i = 0; i < lfsr_len * 2 - 9; ++i){
-		sb[Y_POS - 3][X_POS + 9 + i] = '=';
-	}
-	sb[Y_POS - 3][X_POS + lfsr_len * 2] = '#';
-
 	sb[Y_POS - 4][X_POS + 8] = '+';
 	sb[Y_POS - 3][X_POS + 8] = '<';
 	sb[Y_POS - 2][X_POS] = '+';
@@ -83,9 +78,13 @@ void initScreen(){
 		sb[Y_POS - 2][X_POS + i] = '-';
 	}
 	sb[Y_POS - 2][X_POS + 8] = '+';
-
 	sprintf(sb[Y_POS - 3] + X_POS + 3, "XOR");
 
+	// Taps and input pipes
+	for(int i = 0; i < lfsr_len * 2 - 9; ++i){
+		sb[Y_POS - 3][X_POS + 9 + i] = '=';
+	}
+	sb[Y_POS - 3][X_POS + lfsr_len * 2] = '#';
 	int tap1xpos = X_POS - 1 + lfsr_len * 2 - (tap1 - 1) * 2;
 	sb[Y_POS - 1][tap1xpos] = 'A';
 	if(tap1xpos > X_POS + 8){
@@ -99,6 +98,7 @@ void initScreen(){
 		sb[Y_POS - 3][tap2xpos] = '+';
 	}
 
+	// XOR output display
 	sb[Y_POS - 3][X_POS - 1] = '-';
 	sb[Y_POS - 3][X_POS - 2] = '+';
 	sb[Y_POS - 2][X_POS - 2] = '|';
@@ -107,39 +107,43 @@ void initScreen(){
 	sb[Y_POS][X_POS - 2] = '|';
 	sb[Y_POS + 1][X_POS - 2] = '+';
 	sb[Y_POS + 1][X_POS - 1] = '-';
+
+	// Init displays to "hidden" values
+	for(int i = 0; i < lfsr_len; ++i)
+		sb[Y_POS + 1][X_POS + (lfsr_len - i) * 2 - 1] = '?';
+	sb[Y_POS - 3][X_POS + 2] = '?';
+	sb[Y_POS - 3][X_POS + 6] = '?';
+	sb[Y_POS - 1][X_POS - 2] = '?';
 }
 
 void updateScreen(char randBit, unsigned long step){
 	sprintf(sb[Y_POS + 5] + 2, "Step: %lu", step);
-	sprintf(sb[Y_POS + 6] + 2, "State: %u", lfsr);
+	sprintf(sb[Y_POS + 6] + 2, "State: %u", (lfsr & mask));
 
-	sprintf(sb[Y_POS + 5] + 30, "Tap1: %lu", tap1);
-	sprintf(sb[Y_POS + 6] + 30, "Tap2: %lu", tap2);
+	sprintf(sb[Y_POS + 5] + 30, "Tap1: %u", tap1);
+	sprintf(sb[Y_POS + 6] + 30, "Tap2: %u", tap2);
 
 	if(speed == 0)
 		sprintf(sb[Y_POS + 5] + 40, "Enter - step");
 	else
 		sprintf(sb[Y_POS + 5] + 40, "Speed: %d", speed);
 	sprintf(sb[Y_POS + 6] + 40, "Ctrl + C - quit");
-	// LSB on the right
+
+	// Binary LFSR state, LSB on the right
 	if(!hidden)
 		for(int i = 0; i < lfsr_len; ++i)
 			sb[Y_POS + 1][X_POS + (lfsr_len - i) * 2 - 1] =
 				'0' + ((lfsr >> i) & 1);
+	
+	// XOR display
 	if(!hidden){
-		sb[Y_POS - 3][X_POS + 2] = '0' + ((lfsr >> tap2 - 1) & 1);
-		sb[Y_POS - 3][X_POS + 6] = '0' + ((lfsr >> tap1 - 1) & 1);
+		sb[Y_POS - 3][X_POS + 2] = '0' + ((lfsr >> (tap2 - 1)) & 1);
+		sb[Y_POS - 3][X_POS + 6] = '0' + ((lfsr >> (tap1 - 1)) & 1);
 		sb[Y_POS - 1][X_POS - 2] = '0'
-			+ (((lfsr >> tap1 - 1) ^ (lfsr >> tap2 - 1)) & 1);
+			+ (((lfsr >> (tap1 - 1)) ^ (lfsr >> (tap2 - 1))) & 1);
 	}
-	if(hidden){
-		sb[Y_POS - 3][X_POS + 2] = '?';
-		sb[Y_POS - 3][X_POS + 6] = '?';
-		sb[Y_POS - 1][X_POS - 2] = '?';
-		for(int i = 0; i < lfsr_len; ++i)
-			sb[Y_POS + 1][X_POS + (lfsr_len - i) * 2 - 1] = '?';
-	}
-
+	
+	// LFSR output going offscreen
 	for(int i = SCR_W - 1; i > X_POS + lfsr_len * 2 + 1; --i)
 		sb[Y_POS + 1][i] = sb[Y_POS + 1][i - 1];
 	if(randBit != -1)
@@ -158,15 +162,22 @@ int main(){
 	char menuin = getchar();
 	getchar(); // eat the newline
 	if(menuin == '1'){
+		lfsr_len = 7;
 		lfsr = 0b0001100;
+		tap1 = 2;
+		tap2 = 3;
 	}
 	else if(menuin == '3'){
-		puts("LFSR Toy by CyanBun96 2024");
-		puts("for custom pwn.college dojo");
+		puts("LFSR Toy v1.0 by CyanBun96, 2024");
+		puts("For custom pwn.college dojo");
 		puts("The House Always Wins");
+		puts("License: GPL-3.0");
+		puts("Source repo:");
+		puts("https://github.com/cyanbun96/pwn_college-thaw");
 		return 0;
 	}
 	else if(menuin == '2'){
+		// no checks, have fun :)
 		puts("Register length (2-26):");
 		scanf("%d", &lfsr_len);
 		puts("Tap 1 position (1-Register length):");
@@ -176,15 +187,20 @@ int main(){
 		puts("Initial state in binary (e.g. 1010111):");
 		scanf("%b", &lfsr);
 		puts("Speed (0-11, 0 - manual control):");
-		scanf("%d", &speed);
-		puts("Hide state (1 - yes, 0 - no):");
-		scanf("%d", &hidden);
+		scanf("%hhd", &speed);
+		puts("Hide internal state (1 - yes, 0 - no):");
+		scanf("%hhd", &hidden);
 		getchar(); //eat the newline
 	}
 	else{
 		puts("Invalid input!");
 		return 1;
 	}
+	for(int i = 0; i < lfsr_len; ++i){
+		mask <<= 1;
+		mask |= 1;
+	}
+	lfsr = lfsr & mask;
 	initScreen();
 	updateScreen(-1, 0);
 	drawScreen();
